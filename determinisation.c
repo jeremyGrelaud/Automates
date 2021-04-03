6707d8cc417f3903be76530f7a587f3e3f6ae3f0
jeremyGrelaud <73029242+jeremyGrelaud@users.noreply.github.com>
2021-04-03 15:13:02 +0200

    Delete determinisation.c

--- determinisation.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "automate.h"
-
-int is_asynchronus(automate_t* unAutomate) // return 1 if the automate has 1 epsilon or more
-{
-    return (unAutomate->nbEpsilon != 0);
-}

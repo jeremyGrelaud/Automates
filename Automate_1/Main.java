5e80b6d161f10155174a6dc1c9450c2e2bc7379f
clem908 <73214936+clem908@users.noreply.github.com>
2021-04-10 13:27:35 +0200

    corrections

--- Automate_1/Main.java
+++ Automate_1/Main.java
@@ -1,4 +1,3 @@
-package Automate_1;
 
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -7,37 +6,46 @@ public class Main {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		//Automate automate = FileReader.createAutomateObject("D:/Efrei/L2/java/workspace/Automate/src/Automate_1/B10-8.txt");
-		Automate automate = FileReader.createAutomateObject("D:/Efrei/L2/java/workspace/Automate/src/Automate_1/B10-1.txt");
+		//Automate automate = FileReader.createAutomateObject("D:/Cours EFREI/Semestre 4/Programmation en Java/Automates/src/B10-8.txt");
+		//Automate automate = FileReader.createAutomateObject("D:/Cours EFREI/Semestre 4/Programmation en Java/Automates/src/B10-1.txt");
+		Automate automate = FileReader.createAutomateObject("D:/Cours EFREI/Semestre 4/Programmation en Java/Automates/src/B10-2.txt");
+		//Automate automate = FileReader.createAutomateObject("D:/Cours EFREI/Semestre 4/Programmation en Java/Automates/src/B10-3.txt");
 		
-		//automate.afficher_automate(automate);    
+		Automate copie = new Automate(automate);
 		
-		//automate.standardisation(automate);
+		automate.afficher_automate();
+		//System.out.println(automate.est_standard());
 		
-		//automate.afficher_automate(automate);   
+		//automate.standardisation(); 
 		
-		//automate.est_un_automate_asynchrone(automate);
+		//automate.afficher_automate();   
 		
-		//automate.est_un_automate_deterministe(automate);
+		//automate.est_un_automate_asynchrone();
 		
-		automate.est_un_automate_complet(automate);
+		//automate.est_un_automate_deterministe();
 		
-		automate.completion(automate);
+		//automate.est_un_automate_complet();
+		
+		copie.completion(); 
 		
 		System.out.println("");
-		automate.afficher_automate(automate);
+		copie.afficher_automate();
 		
 		//automate.automate_complementaire(automate);
-		//automate.afficher_automate(automate);
+		//automate.afficher_automate();
 		
 		//System.out.println(automate.reconnaitre("aa", automate));
 		
-		automate.determinisation_et_completion_synchrone(automate);
-		automate.afficher_automate(automate);
+		/*
+		System.out.println("determinisation");
+		Automate deter = automate.determinisation_et_completion_synchrone(automate);
+		deter.afficher_automate(deter);
+		*/
+		
+		//System.out.println(automate.reconnaitre_mot_automate_determinsite("aba"));
+		//automate.afficher_automate();
 
 	}
 	
 
 }
-
-

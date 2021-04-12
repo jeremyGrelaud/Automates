7b0456e52143b250a313ced99ba37e22ceb15c00
jeremyGrelaud <jeremy.grelaud@efrei.net>
2021-04-12 14:04:46 +0200

    Update

--- Automate_1/Main.java
+++ Automate_1/Main.java
@@ -1,52 +1,3 @@
-<<<<<<< Updated upstream
-package Automate_1;
-
-import java.io.FileNotFoundException;
-import java.util.List;
-
-public class Main {
-	
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		//Automate automate = FileReader.createAutomateObject("D:/Efrei/L2/java/workspace/Automate/src/Automate_1/B10-8.txt");
-		//Automate automate = FileReader.createAutomateObject("D:/Efrei/L2/java/workspace/Automate/src/Automate_1/B10-1.txt");
-		Automate automate = FileReader.createAutomateObject("D:/Efrei/L2/java/workspace/Automate/src/Automate_1/B10-2.txt");
-		
-		//automate.afficher_automate(automate);    
-		
-		//automate.standardisation(automate);
-		
-		//automate.afficher_automate(automate);   
-		
-		//automate.est_un_automate_asynchrone(automate);
-		
-		//automate.est_un_automate_deterministe(automate);
-		
-		//automate.est_un_automate_complet(automate);
-		
-		//automate.completion(automate);
-		
-		//System.out.println("");
-		//automate.afficher_automate(automate);
-		
-		//automate.automate_complementaire(automate);
-		//automate.afficher_automate(automate);
-		
-		//System.out.println(automate.reconnaitre("aa", automate));
-		
-		System.out.println("determinisation");
-		Automate deter = automate.determinisation_et_completion_synchrone(automate);
-		deter.afficher_automate(deter);
-		
-		//automate.afficher_automate(automate);
-
-	}
-	
-
-}
-
-
-=======
 package Automate_1;
 
 import java.io.FileNotFoundException;
@@ -61,17 +12,23 @@ public class Main {
 		Automate automate = FileReader.createAutomateObject("D:/Efrei/L2/java/workspace/Automate/src/Automate_1/B10-2.txt");
 		//Automate automate = FileReader.createAutomateObject("D:/Efrei/L2/java/workspace/Automate/src/Automate_1/B10-3.txt");
 		
-		//Automate determinise = FileReader.createAutomateObject("D:/Efrei/L2/java/workspace/Automate/src/Automate_1/B10-1.txt");
+		Automate determinise = FileReader.createAutomateObject("D:/Efrei/L2/java/workspace/Automate/src/Automate_1/B10-1.txt");
 		
 		Automate copie = new Automate(automate);
 		
 		copie.completion();
-		//copie = copie.standardisation();
+		copie.standardisation();
 		//copie.est_un_automate_complet();
 		System.out.println("");
 		copie.afficher_automate();
 		copie.est_un_automate_deterministe();
 		copie.est_un_automate_complet();
+		
+		System.out.println("");
+		copie.automate_complementaire();
+		copie.afficher_automate();
+		
+		determinise.reconnaitre_plusieurs_mot();
 
 		
 		//System.out.println(copie.est_standard());
@@ -124,4 +81,3 @@ public class Main {
 	
 
 }
->>>>>>> Stashed changes
